14bb9166b448dfa62163e20074d093fa9ff50f70
Sahil303 <sahiltamboli303@gmail.com>
2021-09-19 20:53:04 +0530

    Updated Widgets

--- First_gui_window.py
+++ First_gui_window.py
@@ -3,6 +3,26 @@ from tkinter import *
 
 root = tkinter.Tk();
 
+# title
 root.title("My First Window")
 
+# Initializing the size
 root.geometry("800x1000")
+
+# labelling
+label1 = Label(root, text = "First Page")
+label1.pack()
+
+# Buttons
+b1 = Button(root, text = "Click Me")
+b1.place(x = 50, y = 100)
+
+b2 = Button(root, text = "Save Me")
+b2.place(x = 50, y = 150)
+
+b3 = Button(root, text = "Delete")
+b3.place(x = 50, y = 200)
+
+b4 = Button(root, text = "HIT", bg = "#ff0000")
+b4.place(x = 50, y = 250)
+
