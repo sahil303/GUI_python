cc93d30bbe63ea5276b5a136d3fc305f3d31b146
Sahil303 <sahiltamboli303@gmail.com>
2021-09-27 21:19:29 +0530

    demo version buttons added

--- form.py
+++ form.py
@@ -37,3 +37,26 @@ data = ('10th','12th', 'Graduate', 'PG','PHD')
 cd = Combobox(root, values = data)
 cd.place(x=135, y=403)
 
+# Buttons
+b1 = Button(root, text = "SUBMIT", bg = "#0000ff", fg ="#00ff00", width = 10 , height = 1)
+b1.place(x =300, y = 500)
+b2 = Button(root, text = "CANCEL", bg = "#aaaaff", fg ="#000000", width = 10 , height = 1)
+b2.place(x =400, y = 500)
+
+# b3 = Button(root, text = "", bg = "#ffffff", fg ="#222299", width = 10 , height = 1,command = buttonclick)
+# b3.pack()
+# 
+# b4 = Button(root, text = "HIT", bg = "#000000", fg ="#ffffff", width = 10 , height = 1, activebackground = "Orange", activeforeground = "White")
+# b4.place(x = 50, y = 250)
+# 
+# # LEFT RIGHT BOTTOM TOP
+# bb = Button(root, text = "BOTTOM", bg = "#0000ff", fg ="#00ff00", width = 10 , height = 1)
+# bb.pack(side = BOTTOM)
+# 
+# bl = Button(root, text = "LEFT", bg = "#aaaaff", fg ="#000000", width = 10 , height = 1)
+# bl.pack(side = LEFT)
+# 
+# br = Button(root, text = "RIGHT", bg = "#ffffff", fg ="#222299", width = 10 , height = 1)
+# br.pack(side = RIGHT)
+
+root.mainloop()
