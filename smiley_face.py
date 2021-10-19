152419920b6b0718bcae94e737e8f4388a7067ce
Sahil303 <sahiltamboli303@gmail.com>
2021-10-19 22:22:11 +0530

    Completed

--- smiley_face.py
+++ smiley_face.py
@@ -1,27 +1,25 @@
 import  arcade
 
-w  = 800
-h = 800
+w,h = 800,800
+
 arcade.open_window(w,h, "SMILEY FACE")
 arcade.set_background_color(arcade.color.BLACK)
 arcade.start_render()
 
 # Face Circle
-x = 400
-y = 400
-radius = 250
+x,y,radius = 400,400, 250
 arcade.draw_circle_filled(x,y,radius,arcade.color.YELLOW)
 
 # Left eye circle
-x = 310
-y = 500
-radius = 20
+x,y,radius = 310,500, 20
 arcade.draw_circle_filled(x,y,radius,arcade.color.BLACK)
 
 # right eye circle
-x = 480
-y = 500
-radius = 20
-arcade.draw_circle_filled(x,y,radius,arcade.color.BLACK) 
+x,y,radius = 480,500, 20
+arcade.draw_circle_filled(x,y,radius,arcade.color.BLACK)
+
+# Smile line
+x,y,w,h,start_angle,end_angle = 400, 350, 100, 100, 190,350
+arcade.draw_arc_outline(x,y,w,h,arcade.color.BLACK,start_angle,end_angle,15)
 arcade.finish_render()
 arcade.run()
