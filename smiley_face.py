c3778e6bf863544a4d9415ae672866d90a725766
Sahil303 <sahiltamboli303@gmail.com>
2021-10-19 21:53:10 +0530

    bug fixed

--- smiley_face.py
+++ smiley_face.py
@@ -3,7 +3,7 @@ import  arcade
 w  = 800
 h = 800
 arcade.open_window(w,h, "SMILEY FACE")
-arcade.set_baackground_color(arcade.color.BLACK)
+arcade.set_background_color(arcade.color.BLACK)
 arcade.start_render()
 
 arcade.start_render()
